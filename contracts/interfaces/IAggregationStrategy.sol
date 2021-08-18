777afcf7df7cce85c50a04cbcc46f0257bbcc076
TRiLeZ <tyler@trilez.com>
2021-08-18 16:27:16 -0700

    Remove IAggregationStrategy

--- contracts/interfaces/IAggregationStrategy.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity  >=0.5 <0.8;
-
-abstract contract IAggregationStrategy {
-
-    function aggregatePriceAndLiquidity(uint256[] memory prices, uint256[] memory tokenLiquidities, uint256[] memory baseLiquidities) virtual external view
-        returns(uint256 price, uint256 tokenLiquidity, uint256 baseLiquidity);
-
-}
