d5945606d985c2797db53697c5062f7a53231b8d
TRiLeZ <tyler@trilez.com>
2021-09-02 16:12:06 -0700

    Use unchecked math in UniswapV2OracleLibrary

--- contracts/libraries/uniswap-v2-periphery/UniswapV2OracleLibrary.sol
+++ contracts/libraries/uniswap-v2-periphery/UniswapV2OracleLibrary.sol
@@ -10,7 +10,9 @@ library UniswapV2OracleLibrary {
 
     // helper function that returns the current block timestamp within the range of uint32, i.e. [0, 2**32 - 1]
     function currentBlockTimestamp() internal view returns (uint32) {
-        return uint32(block.timestamp % 2**32);
+        unchecked {
+            return uint32(block.timestamp % 2**32);
+        }
     }
 
     // produces the cumulative price using counterfactuals to save gas and avoid a call to sync.
@@ -23,20 +25,22 @@ library UniswapV2OracleLibrary {
             uint32 blockTimestamp
         )
     {
-        blockTimestamp = currentBlockTimestamp();
-        price0Cumulative = IUniswapV2Pair(pair).price0CumulativeLast();
-        price1Cumulative = IUniswapV2Pair(pair).price1CumulativeLast();
+        unchecked {
+            blockTimestamp = currentBlockTimestamp();
+            price0Cumulative = IUniswapV2Pair(pair).price0CumulativeLast();
+            price1Cumulative = IUniswapV2Pair(pair).price1CumulativeLast();
 
-        // if time has elapsed since the last update on the pair, mock the accumulated price values
-        (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = IUniswapV2Pair(pair).getReserves();
-        if (blockTimestampLast != blockTimestamp) {
-            // subtraction overflow is desired
-            uint32 timeElapsed = blockTimestamp - blockTimestampLast;
-            // addition overflow is desired
-            // counterfactual
-            price0Cumulative += uint256(FixedPoint.fraction(reserve1, reserve0)._x) * timeElapsed;
-            // counterfactual
-            price1Cumulative += uint256(FixedPoint.fraction(reserve0, reserve1)._x) * timeElapsed;
+            // if time has elapsed since the last update on the pair, mock the accumulated price values
+            (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = IUniswapV2Pair(pair).getReserves();
+            if (blockTimestampLast != blockTimestamp) {
+                // subtraction overflow is desired
+                uint32 timeElapsed = blockTimestamp - blockTimestampLast;
+                // addition overflow is desired
+                // counterfactual
+                price0Cumulative += uint256(FixedPoint.fraction(reserve1, reserve0)._x) * timeElapsed;
+                // counterfactual
+                price1Cumulative += uint256(FixedPoint.fraction(reserve0, reserve1)._x) * timeElapsed;
+            }
         }
     }
 }
