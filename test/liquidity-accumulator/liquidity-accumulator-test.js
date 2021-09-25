04fedfad58693eb917a238a88900a40c4ce8c0be
Tyler Loewen <tyler@trilez.com>
2021-09-24 19:52:29 -0700

    Add full testing of LiquidityAccumulator#needsUpdate

--- test/liquidity-accumulator/liquidity-accumulator-test.js
+++ test/liquidity-accumulator/liquidity-accumulator-test.js
@@ -5,20 +5,94 @@ const GRT = "0xc944e90c64b2c07662a292be6244bdf05cda44a7";
 
 const TWO_PERCENT_CHANGE = 2000000;
 
-describe("LiquidityAccumulator#needsUpdate", function () {
-    it("Shouldn't need update if delta time is less than the min update delay", async function () {
+describe("LiquidityAccumulator#needsUpdate", () => {
+    const minUpdateDelay = 10000;
+    const maxUpdateDelay = 30000;
+
+    var liquidityAccumulator;
+
+    var updateTime;
+
+    beforeEach(async () => {
         const LiquidityAccumulator = await ethers.getContractFactory("LiquidityAccumulatorHarness");
-        const liquidityAccumulator = await LiquidityAccumulator.deploy(USDC, TWO_PERCENT_CHANGE, 10000, 30000);
+        liquidityAccumulator = await LiquidityAccumulator.deploy(USDC, TWO_PERCENT_CHANGE, minUpdateDelay, maxUpdateDelay);
         await liquidityAccumulator.deployed();
 
         // Configure liquidity
         await liquidityAccumulator.setLiquidity(GRT, 100, 100);
 
+        // Time increases by 1 second with each block mined
+        await hre.timeAndMine.setTimeIncrease(1);
+
         // Initial update
-        await liquidityAccumulator.update(GRT);
+        const updateReceipt = await liquidityAccumulator.update(GRT);
+        updateTime = (await ethers.provider.getBlock(updateReceipt.blockNumber)).timestamp;
+    });
 
-        // TODO: Hardset time to various values and test each
+    it("Shouldn't need update if delta time is less than the min update delay (update threshold passed)", async () => {
+        // Update liquidity (100% change => greater than update threshold)
+        await liquidityAccumulator.setLiquidity(GRT, 200, 200);
 
+        // deltaTime = 1
         expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(false);
+
+        // deltaTime = minUpdateDelay - 1
+        await hre.timeAndMine.setTime(updateTime + minUpdateDelay - 1);
+        expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(false);
+    });
+
+    it("Shouldn't need update if delta time is less than the min update delay (update threshold not passed)", async () => {
+        // deltaTime = 1
+        expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(false);
+
+        // deltaTime = minUpdateDelay - 1
+        await hre.timeAndMine.setTime(updateTime + minUpdateDelay - 1);
+        expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(false);
+    });
+
+    it("Should need update if delta time is within min and max update delay (update threshold passed)", async () => {
+        // Update liquidity (100% change => greater than update threshold)
+        await liquidityAccumulator.setLiquidity(GRT, 200, 200);
+
+        // deltaTime = minUpdateDelay
+        await hre.timeAndMine.setTime(updateTime + minUpdateDelay);
+        expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(true);
+
+        // deltaTime = maxUpdateDelay - 1
+        await hre.timeAndMine.setTime(updateTime + maxUpdateDelay - 1);
+        expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(true);
+    });
+
+    it("Shouldn't need update if delta time is within min and max update delay (update threshold not passed)", async () => {
+        // deltaTime = minUpdateDelay
+        await hre.timeAndMine.setTime(updateTime + minUpdateDelay);
+        expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(false);
+
+        // deltaTime = maxUpdateDelay - 1
+        await hre.timeAndMine.setTime(updateTime + maxUpdateDelay - 1);
+        expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(false);
+    });
+
+    it("Should need update if delta time is >= max update delay (update threshold passed)", async () => {
+        // Update liquidity (100% change => greater than update threshold)
+        await liquidityAccumulator.setLiquidity(GRT, 200, 200);
+
+        // deltaTime = maxUpdateDelay
+        await hre.timeAndMine.setTime(updateTime + maxUpdateDelay);
+        expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(true);
+
+        // deltaTime = maxUpdateDelay + 1
+        await hre.timeAndMine.setTime(updateTime + maxUpdateDelay + 1);
+        expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(true);
+    });
+
+    it("Should need update if delta time is >= max update delay (update threshold not passed)", async () => {
+        // deltaTime = maxUpdateDelay
+        await hre.timeAndMine.setTime(updateTime + maxUpdateDelay);
+        expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(true);
+
+        // deltaTime = maxUpdateDelay + 1
+        await hre.timeAndMine.setTime(updateTime + maxUpdateDelay + 1);
+        expect(await liquidityAccumulator.needsUpdate(GRT)).to.equal(true);
     });
 });
