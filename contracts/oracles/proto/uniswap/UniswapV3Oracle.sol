00541fe0cac10866a38d32ee57556dbea200720c
Tyler Loewen <tyler@trilez.com>
2021-11-05 15:21:22 -0700

    Fix UniswapV3Oracle liquidity problems

--- contracts/oracles/proto/uniswap/UniswapV3Oracle.sol
+++ contracts/oracles/proto/uniswap/UniswapV3Oracle.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
 import "../../PeriodicOracle.sol";
 
 import "../../../libraries/ObservationLibrary.sol";
@@ -41,6 +43,24 @@ contract UniswapV3Oracle is PeriodicOracle {
         return true;
     }
 
+    function calculateAmounts(address pool, uint128 liquidity)
+        internal
+        view
+        returns (uint256 amount0, uint256 amount1)
+    {
+        (uint160 sqrtPriceX96, int24 tick, , , , , ) = IUniswapV3Pool(pool).slot0();
+
+        uint160 sqrtRatioX96A = TickMath.getSqrtRatioAtTick(tick);
+        uint160 sqrtRatioX96B = TickMath.getSqrtRatioAtTick(tick + 1);
+
+        (amount0, amount1) = LiquidityAmounts.getAmountsForLiquidity(
+            sqrtPriceX96,
+            sqrtRatioX96A,
+            sqrtRatioX96B,
+            liquidity
+        );
+    }
+
     function consultFresh(address token)
         internal
         view
@@ -50,30 +70,68 @@ contract UniswapV3Oracle is PeriodicOracle {
             uint256 quoteTokenLiquidity
         )
     {
-        address poolAddress500 = PoolAddress.computeAddress(
-            uniswapFactory,
-            PoolAddress.getPoolKey(token, quoteToken, 500)
-        );
-        address poolAddress3000 = PoolAddress.computeAddress(
-            uniswapFactory,
-            PoolAddress.getPoolKey(token, quoteToken, 3000)
-        );
-        address poolAddress10000 = PoolAddress.computeAddress(
-            uniswapFactory,
-            PoolAddress.getPoolKey(token, quoteToken, 10000)
-        );
-
         WeightedOracleLibrary.PeriodObservation[]
             memory periodObservations = new WeightedOracleLibrary.PeriodObservation[](3);
 
-        if (isContract(poolAddress500))
-            periodObservations[0] = WeightedOracleLibrary.consult(poolAddress500, uint32(period));
+        uint256 total0;
+        uint256 total1;
 
-        if (isContract(poolAddress3000))
-            periodObservations[1] = WeightedOracleLibrary.consult(poolAddress3000, uint32(period));
+        {
+            address poolAddress500 = PoolAddress.computeAddress(
+                uniswapFactory,
+                PoolAddress.getPoolKey(token, quoteToken, 500)
+            );
+            address poolAddress3000 = PoolAddress.computeAddress(
+                uniswapFactory,
+                PoolAddress.getPoolKey(token, quoteToken, 3000)
+            );
+            address poolAddress10000 = PoolAddress.computeAddress(
+                uniswapFactory,
+                PoolAddress.getPoolKey(token, quoteToken, 10000)
+            );
 
-        if (isContract(poolAddress10000))
-            periodObservations[2] = WeightedOracleLibrary.consult(poolAddress10000, uint32(period));
+            if (isContract(poolAddress500)) {
+                periodObservations[0] = WeightedOracleLibrary.consult(poolAddress500, uint32(period));
+
+                (uint256 amount0, uint256 amount1) = calculateAmounts(
+                    poolAddress500,
+                    periodObservations[0].harmonicMeanLiquidity
+                );
+
+                total0 += amount0;
+                total1 += amount1;
+            }
+
+            if (isContract(poolAddress3000)) {
+                periodObservations[1] = WeightedOracleLibrary.consult(poolAddress3000, uint32(period));
+
+                (uint256 amount0, uint256 amount1) = calculateAmounts(
+                    poolAddress3000,
+                    periodObservations[1].harmonicMeanLiquidity
+                );
+
+                total0 += amount0;
+                total1 += amount1;
+            }
+
+            if (isContract(poolAddress10000)) {
+                periodObservations[2] = WeightedOracleLibrary.consult(poolAddress10000, uint32(period));
+
+                (uint256 amount0, uint256 amount1) = calculateAmounts(
+                    poolAddress10000,
+                    periodObservations[2].harmonicMeanLiquidity
+                );
+
+                total0 += amount0;
+                total1 += amount1;
+            }
+        }
+
+        uint128 liquidity = periodObservations[0].harmonicMeanLiquidity +
+            periodObservations[1].harmonicMeanLiquidity +
+            periodObservations[2].harmonicMeanLiquidity;
+
+        require(liquidity != 0, "UniswapV3Oracle: NO_LIQUIDITY");
 
         int24 timeWeightedAverageTick = WeightedOracleLibrary.getArithmeticMeanTickWeightedByLiquidity(
             periodObservations
@@ -86,25 +144,12 @@ contract UniswapV3Oracle is PeriodicOracle {
             quoteToken
         );
 
-        uint128 liquidity = periodObservations[0].harmonicMeanLiquidity +
-            periodObservations[1].harmonicMeanLiquidity +
-            periodObservations[2].harmonicMeanLiquidity;
-
-        // TODO: Better overflow checking
-        require(liquidity >= periodObservations[1].harmonicMeanLiquidity, "UniswapV3DataSource: LIQUIDITY_OVERFLOW");
-
-        uint160 sqrtRatioAX96 = TickMath.getSqrtRatioAtTick(timeWeightedAverageTick);
-        uint160 sqrtRatioBX96 = TickMath.getSqrtRatioAtTick(timeWeightedAverageTick + 1);
-
-        uint256 amount0 = LiquidityAmounts.getAmount0ForLiquidity(sqrtRatioAX96, sqrtRatioBX96, liquidity);
-        uint256 amount1 = LiquidityAmounts.getAmount1ForLiquidity(sqrtRatioAX96, sqrtRatioBX96, liquidity);
-
         if (token < quoteToken) {
-            tokenLiquidity = amount0;
-            quoteTokenLiquidity = amount1;
+            tokenLiquidity = total0;
+            quoteTokenLiquidity = total1;
         } else {
-            tokenLiquidity = amount1;
-            quoteTokenLiquidity = amount0;
+            tokenLiquidity = total1;
+            quoteTokenLiquidity = total0;
         }
     }
 
