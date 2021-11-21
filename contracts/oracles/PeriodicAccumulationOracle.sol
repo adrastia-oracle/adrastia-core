95e89be2157d1486fec1a0a410dd57cbad6a0b9a
Tyler Loewen <tyler@trilez.com>
2021-11-20 19:19:49 -0800

    Add PeriodicAccumulationOracle

---
+++ contracts/oracles/PeriodicAccumulationOracle.sol
@@ -0,0 +1,99 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "./PeriodicOracle.sol";
+import "../interfaces/ILiquidityAccumulator.sol";
+import "../interfaces/IPriceAccumulator.sol";
+
+import "../libraries/AccumulationLibrary.sol";
+import "../libraries/ObservationLibrary.sol";
+
+contract PeriodicAccumulationOracle is PeriodicOracle {
+    address public immutable liquidityAccumulator;
+    address public immutable priceAccumulator;
+
+    mapping(address => AccumulationLibrary.PriceAccumulator) public priceAccumulations;
+    mapping(address => AccumulationLibrary.LiquidityAccumulator) public liquidityAccumulations;
+
+    constructor(
+        address liquidityAccumulator_,
+        address priceAccumulator_,
+        address quoteToken_,
+        uint256 period_
+    ) PeriodicOracle(quoteToken_, period_) {
+        liquidityAccumulator = liquidityAccumulator_;
+        priceAccumulator = priceAccumulator_;
+    }
+
+    function _update(address token) internal override returns (bool) {
+        ObservationLibrary.Observation storage observation = observations[token];
+
+        /*
+         * 1. Update price
+         */
+        {
+            // Always keep the price accumulator up-to-date
+            IPriceAccumulator(priceAccumulator).update(token);
+
+            AccumulationLibrary.PriceAccumulator memory freshAccumulation = IPriceAccumulator(priceAccumulator)
+                .getCurrentAccumulation(token);
+
+            uint256 lastAccumulationTime = priceAccumulations[token].timestamp;
+
+            if (freshAccumulation.timestamp > lastAccumulationTime) {
+                // Accumulator updated, so we update our observation
+
+                if (lastAccumulationTime != 0) {
+                    // We have two accumulations -> calculate price from them
+                    observation.price = IPriceAccumulator(priceAccumulator).calculatePrice(
+                        priceAccumulations[token],
+                        freshAccumulation
+                    );
+                }
+
+                priceAccumulations[token] = freshAccumulation;
+            }
+        }
+
+        /*
+         * 2. Update liquidity
+         */
+        {
+            // Always keep the liquidity accumulator up-to-date
+            ILiquidityAccumulator(liquidityAccumulator).update(token);
+
+            AccumulationLibrary.LiquidityAccumulator memory freshAccumulation = ILiquidityAccumulator(
+                liquidityAccumulator
+            ).getCurrentAccumulation(token);
+
+            uint256 lastAccumulationTime = liquidityAccumulations[token].timestamp;
+
+            if (freshAccumulation.timestamp > lastAccumulationTime) {
+                // Accumulator updated, so we update our observation
+
+                if (lastAccumulationTime != 0) {
+                    // We have two accumulations -> calculate liquidity from them
+                    (observation.tokenLiquidity, observation.quoteTokenLiquidity) = ILiquidityAccumulator(
+                        liquidityAccumulator
+                    ).calculateLiquidity(liquidityAccumulations[token], freshAccumulation);
+                }
+
+                liquidityAccumulations[token] = freshAccumulation;
+            }
+        }
+
+        // Update observation timestamp so that the oracle doesn't update again until the next period
+        observation.timestamp = block.timestamp;
+
+        emit Updated(
+            token,
+            quoteToken,
+            block.timestamp,
+            observation.price,
+            observation.tokenLiquidity,
+            observation.quoteTokenLiquidity
+        );
+
+        return true;
+    }
+}
