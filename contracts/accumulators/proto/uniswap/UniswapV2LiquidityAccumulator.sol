c34c5671da5921232a45614f1ad07434bb0e447d
Tyler Loewen <tyler@trilez.com>
2021-11-24 00:41:33 -0800

    Optimize UniswapV2LiquidityAccumulator - Removes an external call to the factory

--- contracts/accumulators/proto/uniswap/UniswapV2LiquidityAccumulator.sol
+++ contracts/accumulators/proto/uniswap/UniswapV2LiquidityAccumulator.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8;
 
 pragma experimental ABIEncoderV2;
 
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
 import "../../LiquidityAccumulator.sol";
@@ -28,8 +27,7 @@ contract UniswapV2LiquidityAccumulator is LiquidityAccumulator {
         override
         returns (uint256 tokenLiquidity, uint256 quoteTokenLiquidity)
     {
-        // TODO: Inline this
-        address pairAddress = IUniswapV2Factory(uniswapFactory).getPair(token, quoteToken);
+        address pairAddress = pairFor(uniswapFactory, token, quoteToken);
 
         require(pairAddress != address(0), "UniswapV2LiquidityAccumulator: POOL_NOT_FOUND");
 
@@ -44,5 +42,35 @@ contract UniswapV2LiquidityAccumulator is LiquidityAccumulator {
             tokenLiquidity = reserve1;
             quoteTokenLiquidity = reserve0;
         }
+    }
+
+    // returns sorted token addresses, used to handle return values from pairs sorted in this order
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        require(tokenA != tokenB, "UniswapV2Library: IDENTICAL_ADDRESSES");
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), "UniswapV2Library: ZERO_ADDRESS");
+    }
+
+    // calculates the CREATE2 address for a pair without making any external calls
+    function pairFor(
+        address factory,
+        address tokenA,
+        address tokenB
+    ) internal pure returns (address pair) {
+        (address token0, address token1) = sortTokens(tokenA, tokenB);
+        pair = address(
+            uint160(
+                uint256(
+                    keccak256(
+                        abi.encodePacked(
+                            hex"ff",
+                            factory,
+                            keccak256(abi.encodePacked(token0, token1)),
+                            hex"96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f" // init code hash
+                        )
+                    )
+                )
+            )
+        );
     }
 }
