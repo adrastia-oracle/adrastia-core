6e0c335ce7ed636880f1a1b1026c06e5a67decf3
Tyler Loewen <tyler@trilez.com>
2022-01-16 20:43:12 -0800

    Upgrade solc to 0.8.11

--- hardhat.config.js
+++ hardhat.config.js
@@ -56,7 +56,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.10",
+                version: "0.8.11",
                 settings: {
                     optimizer: {
                         enabled: true,
