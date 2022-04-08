7180912014d05228f91d6dc87fe1c099bc0faff0
Tyler Loewen <tyler@trilez.com>
2022-04-08 11:26:24 -0700

    Add QuotationMetadata contract

---
+++ contracts/utils/QuotationMetadata.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier: MIT
+pragma solidity =0.8.11;
+
+import "@openzeppelin-v4/contracts/utils/introspection/IERC165.sol";
+import "@openzeppelin-v4/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+import "../interfaces/IQuoteToken.sol";
+
+contract QuotationMetadata is IQuoteToken, IERC165 {
+    address public immutable quoteToken;
+
+    constructor(address quoteToken_) {
+        quoteToken = quoteToken_;
+    }
+
+    /// @inheritdoc IQuoteToken
+    function quoteTokenName() public view virtual override returns (string memory) {
+        return getStringOrBytes32(quoteToken, IERC20Metadata.name.selector);
+    }
+
+    /// @inheritdoc IQuoteToken
+    function quoteTokenAddress() public view virtual override returns (address) {
+        return quoteToken;
+    }
+
+    /// @inheritdoc IQuoteToken
+    function quoteTokenSymbol() public view virtual override returns (string memory) {
+        return getStringOrBytes32(quoteToken, IERC20Metadata.symbol.selector);
+    }
+
+    /// @inheritdoc IQuoteToken
+    function quoteTokenDecimals() public view virtual override returns (uint8) {
+        return IERC20Metadata(quoteToken).decimals();
+    }
+
+    /// @inheritdoc IERC165
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IQuoteToken).interfaceId;
+    }
+
+    function getStringOrBytes32(address contractAddress, bytes4 selector) internal view returns (string memory) {
+        (bool success, bytes memory result) = contractAddress.staticcall(abi.encodeWithSelector(selector));
+        if (!success) return "";
+
+        return result.length == 32 ? string(abi.encodePacked(result)) : abi.decode(result, (string));
+    }
+}
