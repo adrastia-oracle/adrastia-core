1b19e9689e958a4d11164d7146c61c31ddf01bb0
Tyler Loewen <tyler@trilez.com>
2023-05-26 17:30:33 -0700

    Remove unused Balancer Math library functions

--- contracts/libraries/balancer-v2/Math.sol
+++ contracts/libraries/balancer-v2/Math.sol
@@ -8,73 +8,8 @@ pragma solidity =0.8.13;
 library Math {
     // solhint-disable no-inline-assembly
 
-    /**
-     * @dev Returns the absolute value of a signed integer.
-     */
-    function abs(int256 a) internal pure returns (uint256 result) {
-        // Equivalent to:
-        // result = a > 0 ? uint256(a) : uint256(-a)
-        assembly {
-            let s := sar(255, a)
-            result := sub(xor(a, s), s)
-        }
-    }
-
-    /**
-     * @dev Returns the addition of two unsigned integers of 256 bits, reverting on overflow.
-     */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a + b;
-    }
-
-    /**
-     * @dev Returns the addition of two signed integers, reverting on overflow.
-     */
-    function add(int256 a, int256 b) internal pure returns (int256) {
-        return a + b;
-    }
-
-    /**
-     * @dev Returns the subtraction of two unsigned integers of 256 bits, reverting on overflow.
-     */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a - b;
-    }
-
-    /**
-     * @dev Returns the subtraction of two signed integers, reverting on overflow.
-     */
-    function sub(int256 a, int256 b) internal pure returns (int256) {
-        return a - b;
-    }
-
-    /**
-     * @dev Returns the largest of two numbers of 256 bits.
-     */
-    function max(uint256 a, uint256 b) internal pure returns (uint256 result) {
-        // Equivalent to:
-        // result = (a < b) ? b : a;
-        assembly {
-            result := sub(a, mul(sub(a, b), lt(a, b)))
-        }
-    }
-
-    /**
-     * @dev Returns the smallest of two numbers of 256 bits.
-     */
-    function min(uint256 a, uint256 b) internal pure returns (uint256 result) {
-        // Equivalent to `result = (a < b) ? a : b`
-        assembly {
-            result := sub(a, mul(sub(a, b), gt(a, b)))
-        }
-    }
-
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         return a * b;
-    }
-
-    function div(uint256 a, uint256 b, bool roundUp) internal pure returns (uint256) {
-        return roundUp ? divUp(a, b) : divDown(a, b);
     }
 
     function divDown(uint256 a, uint256 b) internal pure returns (uint256) {
