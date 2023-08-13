3e12e860cc6fe7da35a4f9ef3024d8126d91c6c6
Tyler Loewen <tyler@trilez.com>
2023-08-13 14:15:55 -0700

    Add Aave v3 and Compound Comet supply & borrow accumulators

---
+++ contracts/accumulators/proto/aave/AaveV3SBAccumulator.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.13;
+
+import "../../LiquidityAccumulator.sol";
+import "../../../libraries/SafeCastExt.sol";
+
+abstract contract IAaveV3Pool {
+    struct ReserveData {
+        //stores the reserve configuration
+        uint256 configuration;
+        //the liquidity index. Expressed in ray
+        uint128 liquidityIndex;
+        //the current supply rate. Expressed in ray
+        uint128 currentLiquidityRate;
+        //variable borrow index. Expressed in ray
+        uint128 variableBorrowIndex;
+        //the current variable borrow rate. Expressed in ray
+        uint128 currentVariableBorrowRate;
+        //the current stable borrow rate. Expressed in ray
+        uint128 currentStableBorrowRate;
+        //timestamp of last update
+        uint40 lastUpdateTimestamp;
+        //the id of the reserve. Represents the position in the list of the active reserves
+        uint16 id;
+        //aToken address
+        address aTokenAddress;
+        //stableDebtToken address
+        address stableDebtTokenAddress;
+        //variableDebtToken address
+        address variableDebtTokenAddress;
+        //address of the interest rate strategy
+        address interestRateStrategyAddress;
+        //the current treasury balance, scaled
+        uint128 accruedToTreasury;
+        //the outstanding unbacked aTokens minted through the bridging feature
+        uint128 unbacked;
+        //the outstanding debt borrowed against this asset in isolation mode
+        uint128 isolationModeTotalDebt;
+    }
+
+    function getReserveData(address asset) public view virtual returns (ReserveData memory data);
+}
+
+contract AaveV3SBAccumulator is LiquidityAccumulator {
+    using SafeCastExt for uint256;
+
+    address public immutable aaveV3Pool;
+
+    uint8 internal immutable _liquidityDecimals;
+    uint256 internal immutable _decimalFactor;
+
+    constructor(
+        IAveragingStrategy averagingStrategy_,
+        address aaveV3Pool_,
+        address quoteToken_,
+        uint8 decimals_,
+        uint256 updateTheshold_,
+        uint256 minUpdateDelay_,
+        uint256 maxUpdateDelay_
+    ) LiquidityAccumulator(averagingStrategy_, quoteToken_, updateTheshold_, minUpdateDelay_, maxUpdateDelay_) {
+        aaveV3Pool = aaveV3Pool_;
+
+        _liquidityDecimals = decimals_;
+        _decimalFactor = 10 ** decimals_;
+    }
+
+    function quoteTokenDecimals() public view virtual override(SimpleQuotationMetadata, IQuoteToken) returns (uint8) {
+        return _liquidityDecimals;
+    }
+
+    function liquidityDecimals() public view virtual override returns (uint8) {
+        return _liquidityDecimals;
+    }
+
+    function fetchLiquidity(
+        bytes memory data
+    ) internal view virtual override returns (uint112 tokenLiquidity, uint112 quoteTokenLiquidity) {
+        address token = abi.decode(data, (address));
+        IAaveV3Pool.ReserveData memory reserveData = IAaveV3Pool(aaveV3Pool).getReserveData(token);
+
+        uint256 supply = IERC20Metadata(reserveData.aTokenAddress).totalSupply();
+        uint256 stableDebt = IERC20(reserveData.stableDebtTokenAddress).totalSupply();
+        uint256 variableDebt = IERC20(reserveData.variableDebtTokenAddress).totalSupply();
+        uint256 totalDebt = stableDebt + variableDebt;
+
+        uint256 tokenDecimalsFactor = 10 ** IERC20Metadata(token).decimals();
+
+        tokenLiquidity = ((totalDebt * _decimalFactor) / tokenDecimalsFactor).toUint112();
+        quoteTokenLiquidity = ((supply * _decimalFactor) / tokenDecimalsFactor).toUint112();
+    }
+}
---
+++ scripts/supply-borrow/consult-aave-v3-sb.js
@@ -0,0 +1,188 @@
+const { BigNumber } = require("ethers");
+const hre = require("hardhat");
+
+const ethers = hre.ethers;
+
+const aaveV3Pool = "0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2"; // Aave v3 on mainnet
+
+const wethAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+const usdcAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function createContract(name, ...deploymentArgs) {
+    const contractFactory = await ethers.getContractFactory(name);
+
+    const contract = await contractFactory.deploy(...deploymentArgs);
+
+    await contract.deployed();
+
+    return contract;
+}
+
+async function currentBlockTimestamp() {
+    const currentBlockNumber = await ethers.provider.getBlockNumber();
+
+    return await blockTimestamp(currentBlockNumber);
+}
+
+async function blockTimestamp(blockNum) {
+    return (await ethers.provider.getBlock(blockNum)).timestamp;
+}
+
+async function createOracle(averagingStrategy, aave, quoteToken, period, granularity, liquidityDecimals) {
+    const updateTheshold = 2000000; // 2% change -> update
+    const minUpdateDelay = 5; // At least 5 seconds between every update
+    const maxUpdateDelay = 60; // At most (optimistically) 60 seconds between every update
+
+    const priceAccumulator = await createContract("StaticPriceAccumulator", quoteToken, 2);
+
+    const liquidityAccumulator = await createContract(
+        "AaveV3SBAccumulator",
+        averagingStrategy,
+        aave,
+        quoteToken,
+        liquidityDecimals,
+        updateTheshold,
+        minUpdateDelay,
+        maxUpdateDelay
+    );
+
+    const oracle = await createContract(
+        "PeriodicAccumulationOracle",
+        liquidityAccumulator.address,
+        priceAccumulator.address,
+        quoteToken,
+        period,
+        granularity
+    );
+
+    return {
+        liquidityAccumulator: liquidityAccumulator,
+        priceAccumulator: priceAccumulator,
+        oracle: oracle,
+    };
+}
+
+async function main() {
+    // Periodic oracle parameters
+    const period = 10; // 10 seconds
+    const granularity = 1;
+
+    // Accumulator parameters
+    const averagingStrategy = await createContract("GeometricAveraging");
+    const aave = aaveV3Pool;
+    const quoteToken = ethers.constants.AddressZero;
+    const token = usdcAddress;
+    const liquidityDecimals = 4;
+
+    const oracle = await createOracle(
+        averagingStrategy.address,
+        aave,
+        quoteToken,
+        period,
+        granularity,
+        liquidityDecimals
+    );
+
+    const tokenContract = await ethers.getContractAt("ERC20", token);
+
+    const tokenSymbol = await tokenContract.symbol();
+
+    const updateData = ethers.utils.defaultAbiCoder.encode(["address"], [token]);
+
+    while (true) {
+        try {
+            if (await oracle.priceAccumulator.canUpdate(updateData)) {
+                const price = await oracle.priceAccumulator["consultPrice(address,uint256)"](token, 0);
+                const currentTime = await currentBlockTimestamp();
+
+                const paUpdateData = ethers.utils.defaultAbiCoder.encode(
+                    ["address", "uint", "uint"],
+                    [token, price, currentTime]
+                );
+
+                const updateTx = await oracle.priceAccumulator.update(paUpdateData);
+                const updateReceipt = await updateTx.wait();
+
+                console.log(
+                    "\u001b[" +
+                        93 +
+                        "m" +
+                        "Price accumulator updated. Gas used = " +
+                        updateReceipt["gasUsed"] +
+                        "\u001b[0m"
+                );
+            }
+
+            if (await oracle.liquidityAccumulator.canUpdate(updateData)) {
+                const liquidity = await oracle.liquidityAccumulator["consultLiquidity(address,uint256)"](token, 0);
+                const currentTime = await currentBlockTimestamp();
+
+                console.log(liquidity);
+
+                const laUpdateData = ethers.utils.defaultAbiCoder.encode(
+                    ["address", "uint", "uint", "uint"],
+                    [token, liquidity.tokenLiquidity, liquidity.quoteTokenLiquidity, currentTime]
+                );
+
+                const updateTx = await oracle.liquidityAccumulator.update(laUpdateData);
+                const updateReceipt = await updateTx.wait();
+
+                console.log(
+                    "\u001b[" +
+                        93 +
+                        "m" +
+                        "Liquidity accumulator updated. Gas used = " +
+                        updateReceipt["gasUsed"] +
+                        "\u001b[0m"
+                );
+            }
+
+            if (await oracle.oracle.canUpdate(updateData)) {
+                const updateTx = await oracle.oracle.update(updateData);
+                const updateReceipt = await updateTx.wait();
+
+                console.log(
+                    "\u001b[" + 93 + "m" + "Oracle updated. Gas used = " + updateReceipt["gasUsed"] + "\u001b[0m"
+                );
+            }
+
+            const consultation = await oracle.oracle["consult(address)"](token);
+
+            const tokenLiquidityStr = ethers.utils.commify(
+                ethers.utils.formatUnits(consultation["tokenLiquidity"], liquidityDecimals)
+            );
+
+            const quoteTokenLiquidityStr = ethers.utils.commify(
+                ethers.utils.formatUnits(consultation["quoteTokenLiquidity"], liquidityDecimals)
+            );
+
+            console.log(
+                "\u001b[" + 31 + "m" + "Borrow(%s) = %s, Supply(%s) = %s" + "\u001b[0m",
+                tokenSymbol,
+                tokenLiquidityStr,
+                tokenSymbol,
+                quoteTokenLiquidityStr
+            );
+        } catch (e) {
+            console.log(e);
+        }
+
+        await sleep(1000);
+
+        // Keep mining blocks so that block.timestamp updates
+        await hre.network.provider.send("evm_mine");
+    }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
