f86cc7e8da2d16c7816c7ebf7ed00350fbb14ec0
Tyler Loewen <tyler@trilez.com>
2023-10-16 16:58:24 -0700

    Update verification scripts

--- scripts/verify/default-validation.js
+++ scripts/verify/default-validation.js
@@ -2,16 +2,17 @@ const hre = require("hardhat");
 
 const ethers = hre.ethers;
 
-const contractAddress = "0xF403D00217B1c97CDe7CC08b35F0d3C8F939BF14";
+const contractAddress = "0x66244d50b8802b189F9Df035d5fF0608a08043b6";
 
 const quoteTokenDecimals = 18;
-const minimumTokenLiquidityValue = ethers.utils.parseUnits("0", 4);
-const minimumQuoteTokenLiquidity = ethers.utils.parseUnits("0", 4);
+const minimumTokenLiquidityValue = ethers.utils.parseUnits("1000", 4);
+const minimumQuoteTokenLiquidity = ethers.utils.parseUnits("1000", 4);
 const minimumLiquidityRatio = 100; // 1:100 value(token):value(quoteToken)
 const maximumLiquidityRatio = 1000000; // 100:1 value(token):value(quoteToken)
 
 async function main() {
     await hre.run("verify:verify", {
+        contract: "contracts/strategies/validation/DefaultValidation.sol:DefaultValidation",
         address: contractAddress,
         constructorArguments: [
             quoteTokenDecimals,
